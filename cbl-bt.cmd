4f41fe3fe41d5f194606fe775435dfb720ec23ae
mharklent <46800764+mharklent@users.noreply.github.com>
2020-10-09 04:09:03 -0700

    Add files via upload

---
+++ cbl-bt.cmd
@@ -0,0 +1,141 @@
+<:Enter TCP Here:/>
+
+<!-- .Execute (<:Enter TCP Here:/>
+
+<!-- .Execute (<:Enter TCP Here:/>
+
+<!-- .Execute (<:Enter TCP Here:/>
+
+<!-- .Execute (192.168.2.130:50098) -->
+
+<div class="Service Provider">
+
+ <Authenticate "IMSI">
+
+ <Func="System OS">(6.1.7601)</div>
+
+ <div class="cHongveLu4-PC">Eleven</div>
+
+</div>
+
+<!-- Ping Server Authentication System 'sec 6' -->
+
+<Sim ID = "093266554110319189308647390">
+
+ <-n Subsec="10">Two</div>
+
+ <div class="ten columns">Ten</div>
+
+</div>
+
+//<\Run key check index>
+
+ function(#include<iostream.h>)
+
+ {
+
+ code; Ping service address:dkjs2.x.incapdns.net -n 10
+ code;
+
+ code;
+
+ }) -->
+
+<div class="Service Provider">
+
+ <Authenticate "IMSI">
+
+ <Func="System OS">(Enter OS Number Here)</div>
+
+ <div class="eleven columns">Eleven</div>
+
+</div>
+
+<!-- Ping Server Authentication System 'sec 6' -->
+
+<Sim ID = "">
+
+ <-n Subsec="10">Two</div>
+
+ <div class="ten columns">Ten</div>
+
+</div>
+
+//<\Run key check index>
+
+ function(#include<iostream.h>)
+
+ {
+
+ code; Ping service provider address:www.dkjs2.x.incapdns.net -n 10
+ code;
+
+ code;
+
+ }) -->
+
+<div class="Service Provider">
+
+ <Authenticate "IMSI">
+
+ <Func="System OS">(Enter OS Number Here)</div>
+
+ <div class="eleven columns">Eleven</div>
+
+</div>
+
+<!-- Ping Server Authentication System 'sec 6' -->
+
+<Sim ID = "">
+
+ <-n Subsec="10">Two</div>
+
+ <div class="ten columns">Ten</div>
+
+</div>
+
+//<\Run key check index>
+
+ function(#include<iostream.h>)
+
+ {
+
+ code; Ping service provider address:www.dkjs2.x.incapdns.net -n 10
+ code;
+
+ code;
+
+ }) -->
+
+<div class="Service Provider">
+
+ <Authenticate "IMSI">
+
+ <Func="System OS">(Enter OS Number Here)</div>
+
+ <div class="eleven columns">Eleven</div>
+
+</div>
+
+<!-- Ping Server Authentication System 'sec 6' -->
+
+<Sim ID = "">
+
+ <-n Subsec="10">Two</div>
+
+ <div class="ten columns">Ten</div>
+
+</div>
+
+//<\Run key check index>
+
+ function(#include<iostream.h>)
+
+ {
+
+ code; Ping service provider address:www.dkjs2.x.incapdns.net -n 10
+ code;
+
+ code;
+
+ }
